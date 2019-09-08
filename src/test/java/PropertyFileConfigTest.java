875a62b0b7ce46455e5e8c5adc18d4c04849ec18
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-08 20:13:58 +0200

    Add test which checks for invalid config file

--- src/test/java/PropertyFileConfigTest.java
+++ src/test/java/PropertyFileConfigTest.java
@@ -1,6 +1,7 @@
 
 import com.avrevic.babylon.health.challenge.IConfig;
 import com.avrevic.babylon.health.challenge.PropertyFileConfig;
+import java.io.IOException;
 import java.util.Properties;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -16,6 +17,12 @@ public class PropertyFileConfigTest {
         IConfig config = new PropertyFileConfig();
         Properties properties = config.getConfig(System.getProperty("user.dir") + "\\src\\test\\" + PropertyFileConfig.DEFAULT_CONFIG);
         Assert.assertEquals("https://www.babylonhealth.com", properties.get("website"));
+    }
+
+    @Test(expected = IOException.class)
+    public void invalidPropertyFileShouldThrowException() throws Exception {
+        IConfig config = new PropertyFileConfig();
+        config.getConfig("fail.properties");
     }
 
 }
