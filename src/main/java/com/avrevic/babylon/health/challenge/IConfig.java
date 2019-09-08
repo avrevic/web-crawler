39d811c57b9df5391ee0ca1fd6928e0bcde091bf
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-08 18:15:33 +0200

    Add stub IConfig interface & implementation

---
+++ src/main/java/com/avrevic/babylon/health/challenge/IConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avrevic.babylon.health.challenge;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author avrevic
+ */
+public interface IConfig {
+    
+    public Properties getConfig(String fileName);
+    
+}
---
+++ src/main/java/com/avrevic/babylon/health/challenge/PropertyFileConfig.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avrevic.babylon.health.challenge;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author avrevic
+ */
+public class PropertyFileConfig implements IConfig{
+
+    @Override
+    public Properties getConfig(String fileName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
