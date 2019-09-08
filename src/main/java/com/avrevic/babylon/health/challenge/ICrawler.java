4dfd59f84828ecea9c32c1bfd4dc390a69a687d6
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-08 20:19:28 +0200

    Add stub files for WebCrawler
    Before any implementation, tests will be created

---
+++ src/main/java/com/avrevic/babylon/health/challenge/ICrawler.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avrevic.babylon.health.challenge;
+
+/**
+ *
+ * @author avrevic
+ */
+public interface ICrawler {
+
+    public void crawl(String url);
+
+}
---
+++ src/main/java/com/avrevic/babylon/health/challenge/WebCrawler.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avrevic.babylon.health.challenge;
+
+/**
+ *
+ * @author avrevic
+ */
+public class WebCrawler implements ICrawler {
+
+    @Override
+    public void crawl(String url) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String fetchRobots() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void populateDisabledSites(String robotsFile) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
