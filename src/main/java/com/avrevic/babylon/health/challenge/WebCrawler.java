b8c3ba914732b255776a4186ba38588a6425ffae
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-09 06:34:47 +0200

    Implement fetch robots

--- src/main/java/com/avrevic/babylon/health/challenge/WebCrawler.java
+++ src/main/java/com/avrevic/babylon/health/challenge/WebCrawler.java
@@ -5,6 +5,16 @@
  */
 package com.avrevic.babylon.health.challenge;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
+
 /**
  *
  * @author avrevic
@@ -21,14 +31,22 @@ public class WebCrawler implements ICrawler {
     public void initializeParams(String url) {
         this.url = url;
     }
-    
-    private String fetchRobots() {
 
     @Override
     public void crawl() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    public String fetchRobots() throws MalformedURLException, IOException {
+        URI u = URI.create(this.url);
+        try (InputStream in = u.toURL().openStream()) {
+            File file = new File(System.getProperty("java.io.tmpdir") + "robots.txt");
+            FileUtils.copyInputStreamToFile(in, file);
+            byte[] encoded = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+            return new String(encoded, StandardCharsets.US_ASCII);
+        }
+    }
+
     private void populateDisabledSites(String robotsFile) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
