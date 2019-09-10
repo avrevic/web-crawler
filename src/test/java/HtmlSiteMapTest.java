d89aab240ca6b92ed566a3745eeda45b02360b6d
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-10 22:06:07 +0200

    Add stub tests and test sitemaps

---
+++ src/test/java/HtmlSiteMapTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.Test;
+
+public class HtmlSiteMapTest {
+
+    @Test
+    public void shouldGenerateCorrectHTMLSiteMap() {
+
+    }
+}
---
+++ src/test/java/XmlSiteMapTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.Test;
+
+public class XmlSiteMapTest {
+
+    @Test
+    public void shouldGenerateCorrectXMLSiteMap() {
+
+    }
+}
