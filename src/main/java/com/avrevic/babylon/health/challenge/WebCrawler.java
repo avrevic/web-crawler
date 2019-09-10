19ff3c5a3bcc92f41b157e885b19e7a5e89ef86b
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-10 21:36:53 +0200

    Add more comments

--- src/main/java/com/avrevic/babylon/health/challenge/WebCrawler.java
+++ src/main/java/com/avrevic/babylon/health/challenge/WebCrawler.java
@@ -16,8 +16,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- *
- * @author avrevic
+ * Crawler implementation that gets the sitemap in a hierarchical structure
  */
 public class WebCrawler implements ICrawler {
 
@@ -28,6 +27,7 @@ public class WebCrawler implements ICrawler {
     private UrlUtil urlUtil;
 
     /**
+     * Get the root url which we want to crawl
      *
      * @return
      */
@@ -36,6 +36,7 @@ public class WebCrawler implements ICrawler {
     }
 
     /**
+     * Fetch the list of disabled urls
      *
      * @return
      */
@@ -44,6 +45,7 @@ public class WebCrawler implements ICrawler {
     }
 
     /**
+     * Fetch the sitemap
      *
      * @return
      */
@@ -52,8 +54,9 @@ public class WebCrawler implements ICrawler {
     }
 
     /**
+     * Initialize class params
      *
-     * @param url
+     * @param url url which we want to crawl
      */
     public void initializeParams(String url) {
         this.url = url;
@@ -64,24 +67,27 @@ public class WebCrawler implements ICrawler {
     }
 
     /**
+     * Crawl!
      *
-     * @return @throws Exception
+     * @return Hierarchical list of URLs
+     * @throws Exception
      */
     @Override
     public HashMap<Integer, HashMap<String, Boolean>> crawl() throws Exception {
         this.populateDisabledSites();
-        fetchAllLinks(this.url, 0);
+        fetchAllLinks(this.url);
         return this.getSiteUrls();
     }
 
     /**
+     * Get all links from the website, but first add the source url to the list
+     * if it is not in the list. Recursive function
      *
-     * @param url
-     * @param level
+     * @param url root url from which we want to get the sitemap
      * @throws MalformedURLException
      * @throws IOException
      */
-    public void fetchAllLinks(String url, Integer level) throws MalformedURLException, IOException {
+    public void fetchAllLinks(String url) throws MalformedURLException, IOException {
         Document doc;
         if (!urlUtil.isSourceHostURLEqualTarget(this.url, url)) {
             System.out.println("External link");
@@ -97,10 +103,11 @@ public class WebCrawler implements ICrawler {
             System.out.println("Link crawling is disabled by robots");
             return;
         }
+        // Initialize tha hashmap
         if (this.siteUrls.get(hierachyLevel) == null) {
             HashMap<String, Boolean> newUrlBranch = new HashMap<>();
             newUrlBranch.put(url, Boolean.TRUE);
-            this.siteUrls.put(level, newUrlBranch);
+            this.siteUrls.put(hierachyLevel, newUrlBranch);
         } else {
             this.siteUrls.get(hierachyLevel).put(url, Boolean.TRUE);
         }
@@ -110,17 +117,23 @@ public class WebCrawler implements ICrawler {
         for (Element link : links) {
             String href = link.attr("href");
             try {
+                // See if it is a valid URL - if yes, then it is a fully
+                // qualified URL and not only a path
                 new URL(href);
-                fetchAllLinks(href, level + 1);
+                fetchAllLinks(href);
             } catch (MalformedURLException ex) {
-                fetchAllLinks(this.url + "/" + href, level + 1);
+                // Not a fully qualified url, only a path
+                fetchAllLinks(this.url + "/" + href);
             }
         }
     }
 
     /**
+     * Fetch the robots file so that we can add the disabled urls to the list we
+     * do not want to crawl
      *
-     * @return @throws Exception
+     * @return HTML document
+     * @throws Exception
      */
     public String fetchRobots() throws Exception {
         Document doc = Jsoup.parse(Jsoup.connect(this.url + "/robots.txt").get().toString());
@@ -128,12 +141,15 @@ public class WebCrawler implements ICrawler {
     }
 
     /**
+     * Parse the robots file and add the disabled urls list - we do not want to
+     * crawl them or we might get banned
      *
      * @throws Exception
      */
     public void populateDisabledSites() throws Exception {
         String robotsFile[] = this.fetchRobots().split("\\r?\\n");
         for (String line : robotsFile) {
+            // Find where the slash ('/') starts - from that point we want to get the path of URL
             Integer slashIndex = line.indexOf("/");
             if (slashIndex != -1 && !this.disabledUrls.contains(line.substring(slashIndex))) {
                 this.disabledUrls.add(StringUtils.stripEnd(StringUtils.stripStart(line.substring(slashIndex), "/"), "/"));
