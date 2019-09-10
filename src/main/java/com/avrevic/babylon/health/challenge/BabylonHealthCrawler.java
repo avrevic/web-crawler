398896119ce958b14d77a6cb3f5980e9aaf53910
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-10 21:28:17 +0200

    Add stub for main class

---
+++ src/main/java/com/avrevic/babylon/health/challenge/BabylonHealthCrawler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avrevic.babylon.health.challenge;
+
+/**
+ *
+ * @author avrevic
+ */
+public class BabylonHealthCrawler {
+
+    public static void log(String message) {
+        System.out.println(">>> " + message);
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
