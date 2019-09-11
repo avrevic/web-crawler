3f0bfd1c900fdabb88318bedb8a360cc208920ae
Armin Vrević <armin.vrevic@mstart.hr>
2019-09-11 06:39:25 +0200

    ...

--- src/main/java/com/avrevic/babylon/health/challenge/BabylonHealthCrawler.java
+++ src/main/java/com/avrevic/babylon/health/challenge/BabylonHealthCrawler.java
@@ -8,6 +8,8 @@ package com.avrevic.babylon.health.challenge;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
  *
@@ -30,15 +32,19 @@ public class BabylonHealthCrawler {
         this.crawler = injector.getInstance(ICrawler.class);
         this.siteMap = injector.getInstance(ISiteMap.class);
         this.crawler.initializeParams(url);
-        siteMap.generateSitemap(url, this.crawler.crawl());
+        HashMap<Integer, Set<String>> urlHierarchy = this.crawler.crawl();
+        siteMap.generateSitemap(url, urlHierarchy);
+        this.siteMap = injector.getInstance(XmlSiteMap.class);
+        siteMap.generateSitemap(url, urlHierarchy);
     }
 
     public static void main(String args[]) {
-        if (args[1] == null) {
+        if (args[0] == null) {
             System.out.println("Input url must be set as a first argument of the program");
+            return;
         }
         try {
-            new BabylonHealthCrawler().run(args[1]);
+            new BabylonHealthCrawler().run(args[0]);
         } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
         }
--- src/test/java/BabylonHealthCrawlerTest.java
+++ src/test/java/BabylonHealthCrawlerTest.java
@@ -13,14 +13,14 @@ public class BabylonHealthCrawlerTest {
     @Test
     // Not really much of a test, but want to get 100% code coverage
     public void shouldSilentlyThrowException() {
-        String args[] = new String[2];
+        String args[] = new String[1];
         BabylonHealthCrawler.main(args);
     }
 
     @Test
     public void shouldExecuteProgramSuccessfully() {
-        String args[] = new String[2];
-        args[1] = "http://localhost";
+        String args[] = new String[1];
+        args[0] = "http://localhost";
         BabylonHealthCrawler.main(args);
     }
 
